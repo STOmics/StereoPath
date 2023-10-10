ead55353e4fba87a1523279a5b43778668f113e6
huanghuaqiang <huanghuaqiang@genomics.cn>
2023-10-10 14:08:46 +0800

    update

--- R/STCNV.R
+++ R/STCNV.R
@@ -100,8 +100,8 @@ STCNV <- function(seurat_obj, test_adata, output_dir, gene_order_file, metadata,
 
   write.table(CNV_score, file = "CNV_Score.txt", quote = FALSE, sep = '\t', row.names = T, col.names = T)
 
-  seurat_obj_test = subset(seurat_obj, metadata %in% ref_group_names, invert = T)
-  seurat_obj_test = RenameCells(seurat_obj_test, add.cell.id = patient_id)
+  seurat_obj_test = subset(seurat_obj, cells=colnames(seurat_obj)[seurat_obj@meta.data[, metadata] %in% c("Epi_19N", "Epi_34N")], invert = T)
+  # seurat_obj_test = RenameCells(seurat_obj_test, add.cell.id = patient_id)
 
   fil_seurat_obj_test = subset(seurat_obj_test, cells = rownames(CNV_score))
   fil_seurat_obj_test$cnv_score = CNV_score[colnames(fil_seurat_obj_test), 1]
